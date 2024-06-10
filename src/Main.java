8bae00cd7f9c427fbcc17af9e1e4b02085dc0d7d
borntofall <vsexobnyal@yandex.ru>
2024-06-11 04:34:24 +0600

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        leapYear(1999);
 
+        validType(0, 2024);
+
+        calculateDeliveryDays(50);
+
+    }
+
+    public static void leapYear(int year) {
+        if ((year > 1584 && year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.printf("%d год является високосным \n", year);
+        } else {
+            System.out.printf("%d год не является високосным \n", year);
+
+        }
+    }
+
+    public static void validType(int type, int year) {
+        int currentYear = LocalDate.now().getYear();
+        if (type == 0 && year < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (type == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
+        if (type == 1 && year < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (type == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static int calculateDeliveryDays(int distance) {
+        int deliveryDays = 1;
+        if (distance < 20) {
+            System.out.printf("Потребуется дней на доставку: %s\n", deliveryDays);
+        }
+        if (distance >= 20 && distance < 60) {
+            System.out.printf("Потребуется дней на доставку: %s\n", deliveryDays + 1);
+        }
+        if (distance >= 60 && distance < 100) {
+            System.out.printf("Потребуется дней на доставку : %s\n", deliveryDays + 2);
+        }
+        if (distance >= 100) {
+            System.out.println("Свыше 100 км доставки нет");
+
+        }
+        return deliveryDays;
     }
 }
